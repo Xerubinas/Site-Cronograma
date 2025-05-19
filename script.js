e087cd77821ee49ab7103703ab61ef18f908a73b
Xerubinas <pedrovinicius7007@gmail.com>
2025-05-19 10:22:58 -0300

    att 19/05

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function redirecionar(pagina) {
+
+function toggleTurmas(id) {
+    const div = document.getElementById(id);
+    div.style.display = div.style.display === "flex" ? "none" : "flex";
+  }
+
+  function redirecionar(pagina) {
     window.location.href = pagina;
-}
+  }
